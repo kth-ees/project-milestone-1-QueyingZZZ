ecf48c75181245c9a58a8f75643dcfde86d736da
QueyingZZZ <puhao@kth.se>
2025-09-21 00:13:33 +0200

    Update alu.sv

--- rtl/alu.sv
+++ rtl/alu.sv
@@ -1,16 +1,60 @@
 module alu #(
   BW = 16 // bitwidth
-  ) (
-  input  logic unsigned [BW-1:0] in_a,
-  input  logic unsigned [BW-1:0] in_b,
-  input  logic             [3:0] opcode,
-  output logic unsigned [BW-1:0] out,
-  output logic             [2:0] flags // {overflow, negative, zero}
+  )(
+  input  logic signed [BW-1:0] in_a,
+  input  logic signed [BW-1:0] in_b,
+  input  logic             [2:0] opcode,
+  output logic signed [BW-1:0] out,
+  output logic         [2:0] flags // {overflow, negative, zero}
   );
 
-  // Complete your RTL code here
+always_comb begin
+
+out = 'b0;
+
+case(opcode)
+
+3'b000: out = in_a + in_b;
+3'b001: out = in_a - in_b;
+3'b010: out = in_a & in_b;
+3'b011: out = in_a | in_b;
+3'b100: out = in_a ^ in_b;
+3'b101: out = in_a + 1'b1;
+3'b110: out = in_a;
+3'b111: out = in_b;
+
+default: out = 'b0;
+
+endcase
+
+end
+
+
+always_comb begin
+
+flags = 3'b0;
+
+if (opcode == 3'b000) begin // ADD
+  flags[2] = (in_a[BW-1] == in_b[BW-1]) && (out[BW-1] != in_a[BW-1]);
+end
+else if (opcode == 3'b001) begin // SUB
+  flags[2] = (in_a[BW-1] != in_b[BW-1]) && (out[BW-1] != in_a[BW-1]);
+end
+
+
+flags[1] = out[BW-1];
+
+
+flags[0] = (out == 0);
+
+
+end
 
 endmodule
+
+
+
+
 
 
 
